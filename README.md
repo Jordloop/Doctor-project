2ac934394fe9def25c6684e992e6ebec09728dca
JordLoop <Jordloop@gmail.com>
2017-07-01 14:46:32 -0700

    add asset pipeline, and basic file structure

---
+++ README.md
@@ -0,0 +1,32 @@
+# Code Snippet Database
+
+
+#### **By Jordan Loop**
+
+## Description
+
+
+
+## Behavior and Inputs
+
+
+
+## Setup/Installation Requirements
+# Github
+
+## Known Bugs
+
+* No known bugs
+
+## Support and contact details
+
+If you have any issues or have questions, ideas, concerns, or contributions please contact through Github.
+
+## Technologies Used
+
+
+
+### License
+This software is licensed under the MIT license.
+
+Copyright (c) 2017 Jordan Loop, Epicodus
---
+++ js/doctor-interface.js
@@ -0,0 +1,26 @@
+// const sayHi = require('./../js/doctor.js').sayHiModule;
+//
+// $(() => {
+//   $('#name-form').submit((event) => {
+//     console.log('hello');
+//     event.preventDefault();
+//     const name = $("#input").val();
+//     $("#output").text(sayHi(name));
+//   });
+// });
+
+
+
+
+
+
+
+// var Doctor = require('./../js/doctor.js').doctorModule;
+// var newDoctor = new Doctor();
+//
+// $(document).ready(function(symptom){
+//
+//
+//     // $('#doctor-search').submit(function(event){
+//     //   event.preventDefault();
+// });
