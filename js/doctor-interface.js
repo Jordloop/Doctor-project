3f7e3042f0020c81d5a1dc3d7b53766efb0c319a
JordLoop <Jordloop@gmail.com>
2017-07-04 16:06:48 -0700

    add callback functions for getDoctor() method, callback functions will render content to browser depending on getDoctor results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,17 +1,22 @@
 const Doctor = require('./../js/doctor.js').doctorModule;
 
-const displayDoctors = function(first_name, last_name, image_url) {
-  $("#output").append(`<div class="row">
-                        <div class="col-sm-6 col-md-4">
-                          <div class="thumbnail">
-                            <img src="${image_url}" alt="Doctor's image">
-                            <div class="caption">
-                              <h3>${first_name} ${last_name}</h3>
-                              <p>Esse alterum insolens vis no. Vis an sint propriae prodesset, viris legimus ei mei, ea persius adipisci qui.</p>
-                            </div>
+//Called if API request was fruitful
+const displayDoctors = function(doctor) {
+  $("#output").append(`<div class="col-sm-12 col-md-12">
+                        <div class="thumbnail">
+                          <img src="${doctor.image_url}" alt="Doctor's image">
+                          <div class="caption">
+                            <h3>${doctor.first_name} ${doctor.last_name}</h3>
+                            <hr>
+                            <h4>Bio:</h4>
+                            <p>${doctor.bio}</p>
                           </div>
                         </div>
                       </div>`);
+};
+//Called if API request was not fruitful
+const noResults = function(symptom) {
+  $("#output").append(`<h3>No results to display for "${symptom}".</h3>`);
 };
 
 $(document).ready(function(symptom){
@@ -19,6 +24,7 @@ $(document).ready(function(symptom){
     event.preventDefault();
     const newDoctor = new Doctor();
     const symptom = $('#symptom').val();
-    newDoctor.getDoctors(symptom, displayDoctors);
+    $("#output").empty();
+    newDoctor.getDoctors(symptom, displayDoctors, noResults);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,25 +1,29 @@
 const apiKey = require('./../.env').apiKey;
 
-function Doctor(id, first_name, last_name, image_url){
+function Doctor(id, first_name, last_name, image_url, bio){
   this.id = id;
   this.first_name = first_name;
   this.last_name = last_name;
-  this.img_url = image_url;
+  this.image_url = image_url;
+  this.bio = bio;
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue, display) {
+//Sends request to API. Depending on result of request one of two callback functions is executed.
+Doctor.prototype.getDoctors = function(medicalIssue, successFunction, failFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-
-      for (let i = 0; i < result.data.length; i++) {
-        let first = result.data[i].profile.first_name;
-        let last = result.data[i].profile.last_name;
-        let image = result.data[i].profile.image_url;
-        let newDoctor = new Doctor(i + 1, first, last, image);
-
-        console.log(newDoctor);
-        display(first, last, image);
-
+     if (result.data.length === 0) {
+       failFunction(medicalIssue);
+     }
+     else {
+        for (let i = 0; i < result.data.length; i++) {
+          let first = result.data[i].profile.first_name;
+          let last = result.data[i].profile.last_name;
+          let image = result.data[i].profile.image_url;
+          let bio = result.data[i].profile.bio;
+          let newDoctor = new Doctor(i + 1, first, last, image, bio);
+          successFunction(newDoctor);
+        }
       }
     })
    .fail(function(error){
