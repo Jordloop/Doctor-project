46c48608ae30f9fcc04fa90ec8a5b2c99e08b4f2
JordLoop <Jordloop@gmail.com>
2017-07-04 14:54:36 -0700

    add displayDoctor() function to doctor-interface.js, use displayDoctor() as a callback in getDoctor()

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,24 @@
-var SearchDoctors = require('./../js/doctor.js').doctorModule;
-var newDoctor = new SearchDoctors();
+const Doctor = require('./../js/doctor.js').doctorModule;
+
+const displayDoctors = function(first_name, last_name, image_url) {
+  $("output").append(`<div class="row">
+                        <div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                            <img src="${image_url}" alt="Doctor's image">
+                            <div class="caption">
+                              <h3>${first_name} ${last_name}</h3>
+                              <p>Esse alterum insolens vis no. Vis an sint propriae prodesset, viris legimus ei mei, ea persius adipisci qui.</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`);
+};
 
 $(document).ready(function(symptom){
   $('#symptom-form').submit(function(event){
     event.preventDefault();
-
-    var symptom = $('#symptom').val();
-    console.log(newDoctor.getDoctors(symptom));
-
+    const newDoctor = new Doctor();
+    const symptom = $('#symptom').val();
+    newDoctor.getDoctors(symptom, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,16 +1,27 @@
-var apiKey = require('./../.env').apiKey;
+const apiKey = require('./../.env').apiKey;
 
-function Doctor(){
+function Doctor(id, first_name, last_name, image_url){
+  this.id = id;
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.img_url = image_url;
 }
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      const AllDcotors = [];
-      console.log(result);
-      for (let i = 0; 1 < result.data.length; i++) {
-        console.log(result.data[i].profile.first_name);
+
+      for (let i = 0; i < result.data.length; i++) {
+        let first = result.data[i].profile.first_name;
+        let last = result.data[i].profile.last_name;
+        let image = result.data[i].profile.image_url;
+        let newDoctor = new Doctor(i + 1, first, last, image);
+
+        console.log(newDoctor);
+        displayDoctors(first, last, image);
+
       }
+      return allDoctors;
     })
    .fail(function(error){
       console.log("fail");
