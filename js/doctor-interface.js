a6f2bc96571a8edad57dce840737a2fdb79062c6
JordLoop <Jordloop@gmail.com>
2017-07-04 21:59:40 -0700

    add more bootstraap styling, Doctor constructor now contains phone property

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,17 +2,19 @@ const Doctor = require('./../js/doctor.js').doctorModule;
 
 //Called if API request was fruitful
 const displayDoctors = function(doctor, address) {
-  $("#output").append(`<div class="col-sm-12 col-md-12">
-                        <div class="thumbnail">
-                          <img src="${doctor.image_url}" alt="Doctor's image">
-                          <div class="caption">
-                            <h3>${doctor.first_name} ${doctor.last_name}</h3>
-                            <hr>
-                            <h4>Address: </h4>
-                            <p>${address.street}, ${address.city}, ${address.state} ${address.zip}</p>
+  $("#output").append(` <div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                            <img src="${doctor.image_url}" alt="Doctor's mug">
+                            <div class="caption">
+                              <h3>${doctor.first_name} ${doctor.last_name}</h3>
+                              <hr>
+                              <h5>Address:</h5>
+                              <p>${address.street}, ${address.city}, ${address.state} ${address.zip}</p>
+                              <h5>Phone:</h5>
+                              <p>${doctor.phone}</p>
+                            </div>
                           </div>
-                        </div>
-                      </div>`);
+                        </div>`);
 };
 //Called if API request was not fruitful
 const noResults = function(symptom) {
--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,11 @@
 const apiKey = require('./../.env').apiKey;
 
-function Doctor(id, first_name, last_name, image_url) {
+function Doctor(id, first_name, last_name, image_url, phone) {
   this.id = id;
   this.first_name = first_name;
   this.last_name = last_name;
   this.image_url = image_url;
+  this.phone = phone;
 }
 
 function Address(city, state, street, zip) {
@@ -15,22 +16,17 @@ function Address(city, state, street, zip) {
 }
 
 //Sends request to API. Depending on result of request one of two callback functions is executed.
-Doctor.prototype.getDoctors = function(medicalIssue, successFunction, failFunction) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors, noResults) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=21&user_key=' + apiKey)
    .then(function(result) {
-     console.log(result.data);
      if (result.data.length === 0) {
-       failFunction(medicalIssue);
+       noResults(medicalIssue);
      }
      else {
         for (let i = 0; i < result.data.length; i++) {
-          let newDoctor = new Doctor(i + 1, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.image_url);
-
+          let newDoctor = new Doctor(i + 1, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.image_url, result.data[i].practices[0].phones[0].number);
           let newDoctorAddress = new Address (result.data[i].practices[0].visit_address.city, result.data[i].practices[0].visit_address.state, result.data[i].practices[0].visit_address.street, result.data[i].practices[0].visit_address.zip);
-
-          console.log(newDoctorAddress);
-
-          successFunction(newDoctor, newDoctorAddress);
+          displayDoctors(newDoctor, newDoctorAddress);
         }
       }
     })
